d6e9f99d2fa4d6521fce3a75eaf5b3917d4b9642
DavidOliverius <oliveriusdavid@gmail.com>
2022-04-18 19:46:35 +1000

    release_lookup v1

--- digger.rb
+++ digger.rb
@@ -1,31 +1,32 @@
 require "discogs"
 
 wrapper = Discogs::Wrapper.new("T1A3")
+auth_wrapper = Discogs::Wrapper.new("T1A3", user_token: "FAQInfIhaulwQIEXXKPePjPYAngJoezTNKJuiBFk")
 
-# auth_wrapper = Discogs::Wrapper.new("T1A3", user_token: "FAQInfIhaulwQIEXXKPePjPYAngJoezTNKJuiBFk")
-
-
-# search = auth_wrapper.search(artist_search, :per_page => 10, :type => :artist)
-# artist = wrapper.get_artist("10649353")
-
-# puts "Please enter artist name:"
-# artist_search = gets
-# puts search
-
-# release = wrapper.get_release(21824347)
-# release = wrapper.get_release(1)
-# puts "-------------"
-
-# puts release.keys
-
-# puts "-------------"
-
-# puts "#{release['title']} - #{release['artists'][0]['name']} - #{release['released_formatted']}"
-
+#Grabs release info based on release ID number, returns release Title, Artist and Release date
 def release_info(id)
   wrapper = Discogs::Wrapper.new("T1A3")
   release = wrapper.get_release(id)
-  puts "#{release['title']} by #{release['artists'][0]['name']}, released #{release['released_formatted']}"
+  puts "'#{release['title']}' - #{release['artists'][0]['name']}, released #{release['released_formatted']}"
 end
 
-release_info(5)
+#release_info(x)
+
+def release_lookup(term)
+  auth_wrapper = Discogs::Wrapper.new("T1A3", user_token: "FAQInfIhaulwQIEXXKPePjPYAngJoezTNKJuiBFk")
+  auth_wrapper.search(term, :style => 'tech house', :year => 1992, :per_page => 10, :type => :release)
+end
+
+# search = auth_wrapper.search("maas", :style => 'tech house', :per_page => 10, :type => :release)
+# puts "What would you like to search for?"
+# search_string = gets
+
+puts release_lookup('voulez vous').keys
+
+puts '------------'
+
+puts release_lookup('airflash')['results'][0]['id']
+
+puts '------------'
+
+puts release_info(397017)
