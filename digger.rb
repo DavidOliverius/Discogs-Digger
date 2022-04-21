8a7bba1afff86aaf1adc34b3cd720270d2d8ec1a
DavidOliverius <oliveriusdavid@gmail.com>
2022-04-21 14:54:20 +1000

    Fixed error messages appearing in Terminal

--- digger.rb
+++ digger.rb
@@ -1,5 +1,10 @@
 require "discogs"
 require "tty-prompt"
+require 'colorized_string'
+require 'warning'
+
+# Disables Deprecated warning from Discogs-wrapper
+$VERBOSE = nil
 
 # Disables Hashie error logs from output
 Hashie.logger.level = Logger.const_get 'ERROR'
@@ -8,68 +13,50 @@ prompt = TTY::Prompt.new
 wrapper = Discogs::Wrapper.new("T1A3")
 auth_wrapper = Discogs::Wrapper.new("T1A3", user_token: "FAQInfIhaulwQIEXXKPePjPYAngJoezTNKJuiBFk")
 
-class Response < Hashie::Mash
-    disable_warnings
-  end
-
 #Grabs release info based on release ID number, returns release Title, Artist and Release date
 def release_info(id)
   wrapper = Discogs::Wrapper.new("T1A3")
   release = wrapper.get_release(id)
-  puts "'#{release['title']}' - #{release['artists'][0]['name']}, released #{release['released_formatted']}"
+  "'#{release['title']}' - #{release['artists'][0]['name']}, released #{release['released_formatted']}"
 end
-
-#release_info(x)
-
-# def release_lookup(term)
-#   auth_wrapper = Discogs::Wrapper.new("T1A3", user_token: "FAQInfIhaulwQIEXXKPePjPYAngJoezTNKJuiBFk")
-#   auth_wrapper.search(term, :style => 'tech house', :year => 1992, :per_page => 10, :type => :release)
-# end
-
 
 def release_lookup(term, year, genre)
     auth_wrapper = Discogs::Wrapper.new("T1A3", user_token: "FAQInfIhaulwQIEXXKPePjPYAngJoezTNKJuiBFk")
     auth_wrapper.search(term, :style => genre, :year => year, :per_page => 10, :type => :release, :sort => "have", :sort_order => "dsc").results.first.id
   end
 
-# search = auth_wrapper.search("maas", :style => 'tech house', :per_page => 10, :type => :release)
-# puts "What would you like to search for?"
-# search_string = gets
 
-# puts release_lookup('voulez vous').keys
-
-# puts '------------'
-
-# puts release_lookup('airflash')['results'][0]['id']
-
-# puts '------------'
-
-# puts release_info(397017)
+genres = ["Techno", "Trance", "Progressive House", "Tech House", "Breaks", "Psy-Trance"]
 
 
+    puts ColorizedString['
+    8888888b. 8888888 .d8888b.   .d8888b.   .d88888b.   .d8888b.   .d8888b.     8888888b. 8888888 .d8888b.   .d8888b.  8888888888 8888888b.  
+    888  "Y88b  888  d88P  Y88b d88P  Y88b d88P" "Y88b d88P  Y88b d88P  Y88b    888  "Y88b  888  d88P  Y88b d88P  Y88b 888        888   Y88b 
+    888    888  888  Y88b.      888    888 888     888 888    888 Y88b.         888    888  888  888    888 888    888 888        888    888 
+    888    888  888   "Y888b.   888        888     888 888         "Y888b.      888    888  888  888        888        8888888    888   d88P 
+    888    888  888      "Y88b. 888        888     888 888  88888     "Y88b.    888    888  888  888  88888 888  88888 888        8888888P"  
+    888    888  888        "888 888    888 888     888 888    888       "888    888    888  888  888    888 888    888 888        888 T88b   
+    888  .d88P  888  Y88b  d88P Y88b  d88P Y88b. .d88P Y88b  d88P Y88b  d88P    888  .d88P  888  Y88b  d88P Y88b  d88P 888        888  T88b  
+    8888888P" 8888888 "Y8888P"   "Y8888P"   "Y88888P"   "Y8888P88  "Y8888P"     8888888P" 8888888 "Y8888P88  "Y8888P88 8888888888 888   T88b 
+'].colorize(:yellow)
+    puts ColorizedString["\nWelcome to the Discogs Digger! Use this app to dig for rare and obscured records!"].colorize(:magenta)
 begin
-    puts "
-    ██████╗░██╗░██████╗░█████╗░░█████╗░░██████╗░░██████╗  ██████╗░██╗░██████╗░░██████╗░███████╗██████╗░
-    ██╔══██╗██║██╔════╝██╔══██╗██╔══██╗██╔════╝░██╔════╝  ██╔══██╗██║██╔════╝░██╔════╝░██╔════╝██╔══██╗
-    ██║░░██║██║╚█████╗░██║░░╚═╝██║░░██║██║░░██╗░╚█████╗░  ██║░░██║██║██║░░██╗░██║░░██╗░█████╗░░██████╔╝
-    ██║░░██║██║░╚═══██╗██║░░██╗██║░░██║██║░░╚██╗░╚═══██╗  ██║░░██║██║██║░░╚██╗██║░░╚██╗██╔══╝░░██╔══██╗
-    ██████╔╝██║██████╔╝╚█████╔╝╚█████╔╝╚██████╔╝██████╔╝  ██████╔╝██║╚██████╔╝╚██████╔╝███████╗██║░░██║
-    ╚═════╝░╚═╝╚═════╝░░╚════╝░░╚════╝░░╚═════╝░╚═════╝░  ╚═════╝░╚═╝░╚═════╝░░╚═════╝░╚══════╝╚═╝░░╚═╝
-    "
-    puts "Welcome to the Discogs Digger!"
-    genres = ["Techno", "Trance", "Progressive House", "Tech House", "Breaks", "Psy-Trance"]
-    genre_select = prompt.select("What genre would you like to dig for?", genres)
+    genre_select = prompt.select("\nWhat genre would you like to dig for?", genres, required: true)
 
-    year_select = prompt.slider("Year", min: 1990, max: 2022, step: 1, default: 1990)
+    year_select = prompt.slider("Year", min: 1990, max: 2022, step: 1, default: 1990, required: true)
 
-    id = release_lookup('', year_select, genre_select)
-    puts release_info(id)
+    id = release_lookup('', year_select, genre_select);
+
+    puts "Digger found:"
+    result = release_info(id)
+
+    puts "-----"
+
+    puts result
+
+    puts "-----"
+# Error handling for cases where releases do not exist - eg, Psy-Trance releases in 1990
 rescue NoMethodError => e
-    p "No such release exists with these parameters, please try something else."
-    retry
-    
+    p "No release exists with these parameters, please try something else."
+   
 end
-
-# puts auth_wrapper.search('wiggle', :style => 'tech house', :year => 1996, :per_page => 10, :type => :release)['results'][0]['id']
-# puts "------"
-# puts auth_wrapper.search('', :style => 'techno' && 'trance', :year => 1996, :per_page => 10, :type => :release, :sort => "have", :sort_order => "dsc").results.first.id
