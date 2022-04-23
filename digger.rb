cb79fc085eb4faa8512d8f443c4b519d62ba1761
DavidOliverius <oliveriusdavid@gmail.com>
2022-04-23 13:18:25 +1000

    Search Result written to txt log file

--- digger.rb
+++ digger.rb
@@ -1,6 +1,9 @@
 require 'discogs'
 require 'tty-prompt'
+require 'colorize'
 require 'colorized_string'
+require 'artii'
+require 'io/console'
 
 # Disables Deprecated warning from Discogs-wrapper
 $VERBOSE = nil
@@ -11,12 +14,14 @@ Hashie.logger.level = Logger.const_get 'ERROR'
 prompt = TTY::Prompt.new
 wrapper = Discogs::Wrapper.new('T1A3')
 auth_wrapper = Discogs::Wrapper.new('T1A3', user_token: 'FAQInfIhaulwQIEXXKPePjPYAngJoezTNKJuiBFk')
+logo = Artii::Base.new :font => 'colossal'
+time = Time.new
 
 # Returns string with release info based on release ID number: Title, Artist and Release date
 def release_info(id)
   wrapper = Discogs::Wrapper.new('T1A3')
   release = wrapper.get_release(id)
-  "'#{release['title']}' - #{release['artists'][0]['name']}, released #{release['released_formatted']} - https://www.discogs.com/release/#{id}"
+  "'#{release['title']}' - #{release['artists'][0]['name']}, released #{release['released_formatted']} - https://www.discogs.com/release/#{id} - #{release['styles']}"
 end
 
 # Searches Discogs API for random release ID based upon user input 
@@ -49,18 +54,14 @@ genres = ['Techno', 'House', 'Trance', 'Progressive House', 'Tech House', 'Break
 
 formats = {'Vinyl' => 'Vinyl', 'CD' => 'CD', 'All Formats' => ''}
 
-puts ColorizedString['
-    8888888b. 8888888 .d8888b.   .d8888b.   .d88888b.   .d8888b.   .d8888b.     8888888b. 8888888 .d8888b.   .d8888b.  8888888888 8888888b.
-    888  "Y88b  888  d88P  Y88b d88P  Y88b d88P" "Y88b d88P  Y88b d88P  Y88b    888  "Y88b  888  d88P  Y88b d88P  Y88b 888        888   Y88b
-    888    888  888  Y88b.      888    888 888     888 888    888 Y88b.         888    888  888  888    888 888    888 888        888    888
-    888    888  888   "Y888b.   888        888     888 888         "Y888b.      888    888  888  888        888        8888888    888   d88P
-    888    888  888      "Y88b. 888        888     888 888  88888     "Y88b.    888    888  888  888  88888 888  88888 888        8888888P"
-    888    888  888        "888 888    888 888     888 888    888       "888    888    888  888  888    888 888    888 888        888 T88b
-    888  .d88P  888  Y88b  d88P Y88b  d88P Y88b. .d88P Y88b  d88P Y88b  d88P    888  .d88P  888  Y88b  d88P Y88b  d88P 888        888  T88b
-    8888888P" 8888888 "Y8888P"   "Y8888P"   "Y88888P"   "Y8888P88  "Y8888P"     8888888P" 8888888 "Y8888P88  "Y8888P88 8888888888 888   T88b
-'].colorize(:yellow)
-puts ColorizedString["\nWelcome to the Discogs Digger! Use this app to dig for rare and obscured records!"].colorize(:magenta)
+#File.open("digger_log.txt", "w")
+
+
+$stdout.clear_screen
+puts logo.asciify('DISCOGS DIGGER').colorize(:yellow)
+puts ColorizedString["Welcome to the Discogs Digger! Use this app to dig for rare and obscured records!"].colorize(:magenta)
 begin
+  puts
   format_select = prompt.select("What format would you like?", formats, required:true)
   
   genre_select = prompt.multi_select("\nWhat genre would you like to dig for?", genres, required: true)
@@ -75,7 +76,10 @@ begin
   puts result
   puts '-----'
 
-# Error handling for cases where releases do not exist - eg, Psy-Trance releases in 1990
+File.write("logs/digger_log.txt", "#{time.strftime("%Y-%m-%d %H:%M:%S")} - #{result}\n", mode: "a")
+
+#Error handling for cases where releases do not exist - eg, Psy-Trance releases in 1990
 rescue NoMethodError => e
+  $stdout.clear_screen
   p 'No release exists with these parameters, please try something else.'
 end
