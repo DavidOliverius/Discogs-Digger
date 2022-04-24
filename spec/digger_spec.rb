76d0d81c01c2f562efb5eedd455fee3e63241e90
DavidOliverius <oliveriusdavid@gmail.com>
2022-04-24 17:15:02 +1000

    Rspec attempt - not working

---
+++ spec/digger_spec.rb
@@ -0,0 +1,9 @@
+require relative '../release_info'
+require relative '../release_lookup'
+require relative '../menu'
+
+describe release_lookup do
+  it 'should return ID integer from Discogs API for specific release'
+  id = 21_824_347
+  release_lookup(term, year, genre)
+end
