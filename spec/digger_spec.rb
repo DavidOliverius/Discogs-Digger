770655380409675225d379657cf626466a2ecf52
DavidOliverius <oliveriusdavid@gmail.com>
2022-04-24 17:06:50 +1000

    Deleted Rspec file, testing unsuccessful

--- spec/digger_spec.rb
+++
@@ -1,9 +0,0 @@
-require relative '../release_info'
-require relative '../release_lookup'
-require relative '../menu'
-
-describe release_lookup do
-    it 'should return ID integer from Discogs API for specific release'
-        id = 21824347
-        release_lookup(term, year, genre)
-end
